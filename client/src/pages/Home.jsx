cef295c86634c44ea2658defb5faec6b2f2dca5c
abhishekshukla808 <meabhishekshukla0@gmail.com>
2025-08-01 11:51:03 +0530

    project completed

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navabar from '../component/Navabar'
+import Hero from '../component/Hero'
+import AITools from '../component/AITools'
+import Testimonial from '../component/Testimonial'
+import Plan from '../component/Plan'
+import Footer from '../component/Footer'
+
+const Home = () => {
+  return (
+    <>
+      <Navabar />
+      <Hero />
+      <AITools />
+      <Testimonial />
+      <Plan />
+      <Footer />
+      </>
+  )
+}
+
+export default Home
